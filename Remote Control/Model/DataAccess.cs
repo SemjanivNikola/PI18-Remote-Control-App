d6f291421684519ce3819c6c31fc54d133d146e9
SemjanivNikola <46484614+SemjanivNikola@users.noreply.github.com>
2019-01-14 00:33:48 +0100

    Add files via upload

---
+++ Remote Control/Model/DataAccess.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using Finisar.SQLite;
+//using System.Data.SQLite;
+
+namespace Remote_Control
+{
+    //////SQLite Edition
+    class DataAccess
+    {
+
+        // Connection String for  SQlite Edition
+        static readonly string _ConnectionString = @"Data Source=rcaDB.db;Version=3;New=False;Compress=True";
+        //Data Source=rcaDB.db;Version=3;New=False;Compress=True;
+
+        // Use for ..exe.config file 
+        //   static string _ConnectionString = Sqtlie_project_tutorial.Properties.Settings.Default.SqliteDBtestConnectionString1;
+
+
+
+
+        static SQLiteConnection _Connection = null;
+        public static SQLiteConnection Connection
+        {
+            get
+            {
+                if (_Connection == null)
+                {
+                    _Connection = new SQLiteConnection(_ConnectionString);
+                    _Connection.Open();
+
+                    return _Connection;
+                }
+                else if (_Connection.State != System.Data.ConnectionState.Open)
+                {
+                    _Connection.Open();
+
+                    return _Connection;
+                }
+                else
+                {
+                    return _Connection;
+                }
+            }
+        }
+
+        public static void ConnectionClose()
+        {
+            Connection.Close();
+        }
+
+        /*public static DataView GetDataTableView(string sql)
+        {
+            DataSet ds = GetDataView(sql);
+            DataView dv = new DataView(ds.Tables[0]);
+
+            return dv;
+        }*/
+
+        public static DataSet GetDataSet(string sql)
+        {
+            SQLiteCommand cmd = new SQLiteCommand(sql, Connection);
+            SQLiteDataAdapter adp = new SQLiteDataAdapter(cmd);
+
+            DataSet ds = new DataSet();
+            adp.Fill(ds);
+            Connection.Close();
+
+            cmd.Dispose();
+            adp.Dispose();
+
+            return ds;
+        }
+
+        public static DataTable GetDataTable(string sql)
+        {
+            Console.WriteLine(sql);
+            DataSet ds = GetDataSet(sql);
+
+            if (ds.Tables.Count > 0)
+                return ds.Tables[0];
+            return null;
+        }
+
+        /*public static DataTable GetDataView(string sql)
+        {
+            SQLiteCommand cmd = new SQLiteCommand(sql, Connection);
+            SQLiteDataAdapter adp = new SQLiteDataAdapter(cmd);
+            DataTable dt = new DataTable();
+        
+            adp.Fill(dt);
+            Connection.Close();
+            return dt;
+        }*/
+
+        public static int ExecuteSQL(string sql)
+        {
+            SQLiteCommand cmd = new SQLiteCommand(sql, Connection);
+            return cmd.ExecuteNonQuery();
+        }
+
+        /*public static DataTable GetDataTableView(string sql)
+        {
+            SQLiteCommand cmd = new SQLiteCommand(sql, Connection);
+            cmd.CommandText = String.Format(sql);
+
+            SQLiteDataAdapter adp = new SQLiteDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            DataSet ds = new DataSet();
+
+            try
+            {
+                ds.Tables.Add(dt);
+                adp.Fill(dt);
+                Connection.Close();
+            }
+
+            catch (Exception)
+            {
+                throw;
+            }
+
+            if (ds.Tables.Count > 0)
+                return ds.Tables[0];
+            return null;
+        }*/
+
+        //  Izvlačenje serijskog broja uređaja iz baze
+        public static string GetSN(string sql, int position)
+        {
+            string strSN = null;
+            int br = 0;
+            SQLiteCommand cmd = new SQLiteCommand(sql, Connection);
+            SQLiteDataReader reader = cmd.ExecuteReader();
+
+            try
+            {
+                while (reader.Read() && br <= position)
+                {
+                    strSN = reader.GetValue(0).ToString();
+                    br++;
+                }
+            }
+
+            catch (Exception)
+            {
+                throw;
+            }
+
+            cmd.Dispose();
+            reader.Dispose();
+
+            return strSN;
+        }
+    }
+
+}
---
+++ Remote Control/Model/TableFill.cs
@@ -0,0 +1,221 @@
+﻿using Finisar.SQLite;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Remote_Control
+{
+    class TableFill : DataAccess
+    {
+
+        static string sqlQuery = null;
+
+        //-----   CLASS VARS   -----//
+        //-----   LIGHTS   -----//
+        public static string brightness = null;
+
+        //-----   TV   -----//
+        public static string vol = null;
+        public static string program = null;
+
+        //-----   AIR CONDITIONER   -----//
+        public static string fan_speed = null;
+        
+        //-----   WATER HEATER   -----//
+        public static string co2 = null;
+
+        //-----   WASHING MACHINE   -----//
+        //-----   DISHWASHER   -----//
+
+        //-----   STOVE   -----//
+        public static string stoveTemp = null;
+        public static string p1Temp = null;
+        public static string p2Temp = null;
+        public static string p3Temp = null;
+        public static string p4Temp = null;
+        public static string p5Temp = null;
+
+        //-----   REFIGERATOR   -----//
+
+
+
+
+        //-----   UNIVERSAL VARS   -----//
+        public static string inUse = null;
+        public static string name = null;
+        public static string model = null;
+        public static string off_timer = null;
+        public static string temp = null;
+        public static string delay_start = null;
+        public static string sn = null;
+
+
+        public static void GetProperties(string devName, string sqlQuery)
+        {
+            SQLiteCommand cmd = new SQLiteCommand(sqlQuery, Connection);
+            SQLiteDataReader reader = cmd.ExecuteReader();
+
+            try
+            {
+                switch (devName)
+                {
+                    case "Lights":
+                        {
+                            while (reader.Read())
+                            {
+                                brightness =(string) reader["brightness"];
+                                Convert.ToInt32(brightness);
+                                sn = reader["sn"].ToString();
+                                inUse = (string) reader["in_use"];
+                                name = (string) reader["name"];
+                                model = (string) reader["model"];
+                            }
+                        }
+                        break;
+                    /*case "TV":
+                        {
+                            vol = (string)reader["vol"];
+                            program = (string)reader["program"];
+                            sn = (string)reader["sn"];
+                            inUse = (string)reader["in_use"];
+                            name = (string)reader["name"];
+                            model = (string)reader["model"];
+                        }
+                        break;*/
+                    case "AirCon":
+                        {
+                            temp = (string)reader["temp"];
+                            delay_start = (string)reader["delay_start"];
+                            fan_speed = (string)reader["fan_speed"];
+                            sn = (string)reader["sn"];
+                            inUse = (string)reader["in_use"];
+                            name = (string)reader["name"];
+                            model = (string)reader["model"];
+                        }
+                        break;
+                    case "WtrHtr":
+                        {
+                            temp = (string)reader["temp"];
+                            co2 = (string)reader["co2"];
+                            sn = (string)reader["sn"];
+                            inUse = (string)reader["in_use"];
+                            name = (string)reader["name"];
+                            model = (string)reader["model"];
+                        }
+                        break;
+                    case "WasMachine":
+                        {
+                            temp = (string)reader["temp"];
+                            delay_start = (string)reader["delay_start"];
+                            sn = (string)reader["sn"];
+                            inUse = (string)reader["in_use"];
+                            name = (string)reader["name"];
+                            model = (string)reader["model"];
+                        }
+                        break;
+                    case "DisWasher":
+                        {
+                            delay_start = (string)reader["delay_start"];
+                            sn = (string)reader["sn"];
+                            inUse = (string)reader["in_use"];
+                            name = (string)reader["name"];
+                            model = (string)reader["model"];
+                        }
+                        break;
+                    case "Stove":
+                        {
+                            stoveTemp = (string)reader["stoveTemp"];
+                            p1Temp = (string)reader["p1Temp"];
+                            p2Temp = (string)reader["p2Temp"];
+                            p3Temp = (string)reader["p3Temp"];
+                            p4Temp = (string)reader["p4Temp"];
+                            p5Temp = (string)reader["p5Temp"];
+                            sn = (string)reader["sn"];
+                            inUse = (string)reader["in_use"];
+                            name = (string)reader["name"];
+                            model = (string)reader["model"];
+                        }
+                        break;
+                    case "Refri":
+                        {
+                            temp = (string)reader["temp"];
+                            sn = (string)reader["sn"];
+                            inUse = (string)reader["in_use"];
+                            name = (string)reader["name"];
+                            model = (string)reader["model"];
+                        }
+                        break;
+                    default: break;
+                }
+                cmd.Dispose();
+                reader.Dispose();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public static void Trigger(string type, string name, string sn, string model)
+        {
+            try
+            {
+                switch (type)
+                {
+                    case "Lights":
+                        {
+                            sqlQuery = " INSERT INTO Lights (brightness, sn, in_use, name, model ) VALUES (0, '" + sn + "', 0, '" + name + "', '" + model + "') ";
+                        }
+                        break;
+                    case "TV":
+                        {
+                            sqlQuery = " INSERT INTO TV (vol, program, off_timer, sn, in_use, name, model ) VALUES (0, 0, 0, '" + sn + "', 0, '" + name + "', '" + model + "') ";
+                        }
+                        break;
+                    case "Air Conditioner":
+                        {
+                            sqlQuery = " INSERT INTO AirCon (delay_start, off_timer, temp, fan_speed, sn, in_use, name, model ) VALUES (0, 0, 0, 0, '"+ sn + "', 0, '" + name + "', '" + model + "') ";
+                        }
+                        break;
+                    case "Water Heater":
+                        {
+                            sqlQuery = " INSERT INTO WtrHtr (temp, co2, sn, in_use, name, model ) VALUES (0, 0, '" + sn + "', 0, '" + name + "', '" + model + "') ";
+                        }
+                        break;
+                    case "Washing Machine":
+                        {
+                            sqlQuery = " INSERT INTO WasMachine (delay_start, temp, sn, in_use, name, model ) VALUES (0, 0, '" + sn + "', 0, '" + name + "', '" + model + "') ";
+                        }
+                        break;
+                    case "Dishwasher":
+                        {
+                            sqlQuery = " INSERT INTO DisWasher (delay_start, sn, in_use, name, model ) VALUES (0, '" + sn + "', 0, '" + name + "', '" + model + "') ";
+                        }
+                        break;
+                    case "Stove":
+                        {
+                            sqlQuery = " INSERT INTO Stove (NumOfPlates, stoveTemp, PlateNum, in_use, sn, name, model, p1Temp, p2Temp, p3Temp, p4Temp, p5Temp) VALUES (0, 0, 0, 0, '" + sn + "', '" + name + "', '" + model + "', 0, 0, 0, 0, 0) ";
+                        }
+                        break;
+                    case "Refrigirator":
+                        {
+                            sqlQuery = " INSERT INTO Refri (temp, sn, in_use, name, model ) VALUES (0, '" + sn + "', 0, '" + name + "', '" + model + "') ";
+                        }
+                        break;
+                    default: break;
+                }
+
+                ExecuteSQL(sqlQuery);
+                ConnectionClose();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
