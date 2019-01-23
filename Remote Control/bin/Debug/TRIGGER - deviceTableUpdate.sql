0a4a27319e43f0d00d10a75ee5b3aa683c54107b
SemjanivNikola <46484614+SemjanivNikola@users.noreply.github.com>
2019-01-23 14:28:20 +0100

    Delete TRIGGER - deviceTableUpdate.sql

--- Remote Control/bin/Debug/TRIGGER - deviceTableUpdate.sql
+++
@@ -1,8 +0,0 @@
-CREATE TRIGGER update_wasMachine_table 
-AFTER INSERT ON device 
-FOR EACH ROW 
-BEGIN 
-	INSERT INTO WasMachine 
-	SELECT sn, type, name, model, date, time, pass_tpe, pass 
-	FROM device WHERE type = 'Washing Machine'; 
-END
